8ff3a0c4f8ddefef1674ddf6de86ea105ac8627e
Gabriel D Minzemalulu <gabriel.CSCU.2018@gmail.com>
2024-04-23 10:33:51 +0300

    iniit

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return(
+        <nav className="w-full fixed top-o left-0 py-6 border-b bg-white z-10 ">
+            Test
+        </nav>
+    )
+}
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { Benne } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -8,10 +9,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'airbnb': '#ff385c',
+        'airbnb-dark': '#d50027'
       },
     },
   },
